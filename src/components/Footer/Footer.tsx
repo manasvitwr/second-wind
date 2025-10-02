d0891267b8001e33a8ed2c9cfe9d510582bed33f
Manasvi Tiwari <manasvi.tiwari@proton.me>
2025-10-02 21:11:20 +0530

    feat(ui): styling changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,9 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className="mt-auto py-4 md:py-3 px-0 w-full text-center border-t border-neutral-800 bg-gradient-to-b from-transparent to-[#000000]">
+    <footer className="mt-auto py-1 md:py-3 px-0 w-screen -mx-4 text-center border-t border-neutral-800 bg-gradient-to-b from-transparent to-[#000000]">
       <div className="footer-text text-[15px] md:text-base text-neutral-500 mb-2">
-        / Made with <span className="text-red-500">♥️</span> & caffeine // Manasvi /
+        / made w <span className="text-red-500">♥️</span> & caffeine // Manasvi /
       </div>
       <div className="flex justify-center items-center gap-1 mt-2 -mb-2">
         <a 
